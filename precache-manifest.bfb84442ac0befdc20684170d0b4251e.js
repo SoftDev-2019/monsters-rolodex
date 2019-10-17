053d03e6b27fb7976914a88bd2ba95f051358cd8
SoftDev-2019 <shaneaparkerson@icloud.com>
2019-10-16 21:22:04 -0500

    Updates

---
+++ precache-manifest.bfb84442ac0befdc20684170d0b4251e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e6359b659188513515dc1ec3383e495",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "12058a0e8ed5f1ade7b5",
+    "url": "/monsters-rolodex/static/css/main.0f1d2373.chunk.css"
+  },
+  {
+    "revision": "0e24f7de5d98616ce093",
+    "url": "/monsters-rolodex/static/js/2.1622286a.chunk.js"
+  },
+  {
+    "revision": "12058a0e8ed5f1ade7b5",
+    "url": "/monsters-rolodex/static/js/main.75d0cb5e.chunk.js"
+  },
+  {
+    "revision": "27b815a390445e67878e",
+    "url": "/monsters-rolodex/static/js/runtime-main.b1e02040.js"
+  }
+]);
